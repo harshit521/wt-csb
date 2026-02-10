5bebbe407b2aae6d63bd9e81daf7ca3aa2612a5e
Harshit kumar <harshitkumar30012006@gmail.com>
2026-02-10 12:28:18 +0530

    async

---
+++ async/async.js
@@ -0,0 +1,36 @@
+// function aa(){
+//   console.log("first")
+// }
+// setTimeout(aa,4000);
+
+// let count =0;
+// let id = setInterval(()=>{console.log("first"); if(count==5) clearInterval(id);count++ },2000)
+
+// function roll(num,next,delay){
+//   setTimeout(()=>{
+//     console.log("Roll no is "+ num);
+//     if(next) next();
+//   },delay)
+// }
+
+// roll(1212,()=>{
+//   console.log("loading....");
+//   roll(1213,()=>{
+//     console.log("loading....");
+//     roll(1214,()=>{
+//       console.log("it is getting over");
+//       roll(1215);
+//     },3000);
+//   },2000);
+
+// },1000);
+
+
+let uiTeam = ["a","b","c","d"];
+let flutterTeam = ["w","x","y","z"];
+
+function showMenu(){
+  
+}
+
+
